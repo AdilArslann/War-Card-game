7c8dc1998009ee23153701c23b563623abf2d50c
MehmetTop360 <mehmettop2003@gmail.com>
2023-05-14 20:49:45 +0300

    Issue #5 and #6

--- war_game.py
+++ war_game.py
@@ -1,6 +1,30 @@
 from deck import Deck
 from player import Player
 
+def handle_war(player1, player2):
+    war_pile = []
+    while True:
+        player1_cards = [player1.play_card() for _ in range(3)]
+        player2_cards = [player2.play_card() for _ in range(3)]
+
+        if not player1_cards or not player2_cards:
+            break
+
+        war_pile.extend(player1_cards)
+        war_pile.extend(player2_cards)
+
+        top_card1 = player1_cards[-1]
+        top_card2 = player2_cards[-1]
+
+        if top_card1 > top_card2:
+            player1.hand.extend(war_pile)
+            break
+        elif top_card1 < top_card2:
+            player2.hand.extend(war_pile)
+            break
+        else:
+            continue
+
 def play_round(player1, player2):
     card1 = player1.play_card()
     card2 = player2.play_card()
@@ -13,6 +37,14 @@ def play_round(player1, player2):
         player2.receive_card(card2)
     else:
         print("Tie!")
+
+def display_winner(player1, player2):
+    if len(player1.hand) > len(player2.hand):
+        print(f"{player1} wins the game!")
+    elif len(player1.hand) < len(player2.hand):
+        print(f"{player2} wins the game!")
+    else:
+        print("It's a draw!")
 
 def play_game():
     deck = Deck()
