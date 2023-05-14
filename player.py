d1b9cdd6eef60590e45f079d393aae01b49a896c
ufuksaylan <ufuksaylannn@gmail.com>
2023-05-14 16:07:19 +0300

    update player

--- player.py
+++ player.py
@@ -1,3 +1,13 @@
 class Player:
     def __init__(self, name):
         self.name = name
+        self.hand = []
+
+    def __repr__(self):
+        return self.name
+
+    def receive_card(self, card):
+        self.hand.append(card)
+
+    def play_card(self):
+        return self.hand.pop(0)
