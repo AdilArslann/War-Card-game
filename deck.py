159a1690975c7384322073c29e85e5ba184a7830
Adil <adilarslan3@gmai.com>
2023-05-14 14:21:58 +0300

    Initialize a standard 52-card deck #2

--- deck.py
+++ deck.py
@@ -1,3 +1,15 @@
+import random
+
 class Deck:
     def __init__(self):
-        pass
+        self.cards = self.generate_deck()
+        self.shuffle()
+
+    def generate_deck(self):
+        ranks = list(range(2, 11)) + ["Jack", "Queen", "King", "Ace"]
+        suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+        deck = [Card(rank, suit) for rank in ranks for suit in suits]
+        return deck
+
+    def shuffle(self):
+        random.shuffle(self.cards)
